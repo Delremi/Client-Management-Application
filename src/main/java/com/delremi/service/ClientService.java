17215a7153cd79f44405eca9d8d4aa63382f1eba
Del Remi Liik <liik.delremi@gmail.com>
2021-12-29 23:08:08 +0200

    add entity creation and update logging

--- src/main/java/com/delremi/service/ClientService.java
+++ src/main/java/com/delremi/service/ClientService.java
@@ -6,12 +6,14 @@ import com.delremi.model.Client;
 import com.delremi.model.User;
 import com.delremi.repository.ClientRepository;
 import com.delremi.security.AuthUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class ClientService {
 
     @Autowired
@@ -31,7 +33,8 @@ public class ClientService {
         int userId = AuthUtils.getAuthenticatedUserId();
         User user = userService.getUser(userId);
         client.setCreatedBy(user);
-        clientRepository.save(client);
+        Client result = clientRepository.save(client);
+        log.info("Saved Client with ID " + result.getId());
     }
 
     public List<Client> getClients() throws EntityNotFoundException {
@@ -57,5 +60,6 @@ public class ClientService {
         client.setAddress(clientCreationDto.getAddress());
         client.setCountry(clientCreationDto.getCountry());
         clientRepository.save(client);
+        log.info("Updated Client with ID " + id);
     }
 }
--- src/main/java/com/delremi/service/UserService.java
+++ src/main/java/com/delremi/service/UserService.java
@@ -3,11 +3,13 @@ package com.delremi.service;
 import com.delremi.exception.EntityNotFoundException;
 import com.delremi.model.User;
 import com.delremi.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -17,7 +19,9 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
 
     public User saveUser(String username, String password) {
-        return userRepository.save(new User(null, username, passwordEncoder.encode(password)));
+        User result = userRepository.save(new User(null, username, passwordEncoder.encode(password)));
+        log.info("Saved User with ID " + result.getId());
+        return result;
     }
 
     public User getUser(int id) throws EntityNotFoundException {
