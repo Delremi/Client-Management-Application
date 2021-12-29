a3337f3ddbca3c40a6738c483147f420eb517389
Del Remi Liik <liik.delremi@gmail.com>
2021-12-29 22:06:27 +0200

    check name for digits, unify email format error message

--- src/main/java/com/delremi/dto/ClientCreationDto.java
+++ src/main/java/com/delremi/dto/ClientCreationDto.java
@@ -3,27 +3,28 @@ package com.delremi.dto;
 import com.delremi.model.Country;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ClientCreationDto {
 
     @NotBlank(message = "First name must not be empty!")
+    @Pattern(regexp = "^$|^[A-Za-züõöäÜÕÖÄ]+$", message = "First name must contain only characters!")
     private String firstName;
 
     @NotBlank(message = "Last name must not be empty!")
+    @Pattern(regexp = "^$|^[A-Za-züõöäÜÕÖÄ]+$", message = "Last name must contain only characters!")
     private String lastName;
 
     @NotBlank(message = "Username must not be empty!")
     private String username;
 
+    @Email(message = "Invalid email address format!")
     private String email;
 
     @NotBlank(message = "Address must not be empty!")
